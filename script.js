c95a1160f667ea8e983d58f2bfcd95d4c47fc06d
Paulette Zaldivar Flores <paulettz@icloud.com>
2022-11-05 13:31:01 +0900

    refactor: installed GA

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 var scrollSpy = new bootstrap.ScrollSpy(document.body, {
   target: '#about-me'
 })
+
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-V84L5B24EH');
